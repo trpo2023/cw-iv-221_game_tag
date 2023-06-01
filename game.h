a87f5ec2812085333320f345efd49f9478a53b55
plesha <124110301+p5tmm@users.noreply.github.com>
2023-06-01 15:11:55 +0600

    Update game.h

--- game.h
+++ game.h
@@ -1,8 +1,6 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include <time.h>
-
 #define SIZE 4
 
 void printBoard(int board[SIZE][SIZE]);
@@ -10,6 +8,5 @@ void swap(int* a, int* b);
 void shuffle(int board[SIZE][SIZE]);
 int checkWin(int board[SIZE][SIZE]);
 void move(int board[SIZE][SIZE], char command);
-void displayTimeAndMoves(time_t startTime, int moves);
 
 #endif
