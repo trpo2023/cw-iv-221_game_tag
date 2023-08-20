08deb7ed813bc43102aed78a2625d8fdfc11e6c3
plesha <124110301+p5tmm@users.noreply.github.com>
2023-08-20 20:06:30 +0600

    Updated game.c
    Added a function to display the number of moves.
    Updated the function for shuffling the game board.

--- game.c
+++ game.c
@@ -1,10 +1,10 @@
 #include "game.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 #define SIZE 4
 
+// Функция для вывода игровой доски на экран
 void printBoard(int board[SIZE][SIZE]) {
     printf("┌─────┬─────┬─────┬─────┐\n");
     for (int i = 0; i < SIZE; i++) {
@@ -23,20 +23,32 @@ void printBoard(int board[SIZE][SIZE]) {
     printf("└─────┴─────┴─────┴─────┘\n\n");
 }
 
+// Функция для обмена значениями двух переменных
 void swap(int* a, int* b) {
     int t = *a;
     *a = *b;
     *b = t;
 }
 
+// Функция для перемешивания игровой доски
 void shuffle(int board[SIZE][SIZE]) {
-    srand(time(0));
     for (int i = SIZE * SIZE - 1; i > 0; i--) {
         int j = rand() % (i + 1);
-        swap(&board[0][i], &board[0][j]);
+
+        // Преобразование индекса i в строку и столбец
+        int row_i = i / SIZE; // Получаем номер строки, где находится элемент с индексом i                               
+        int col_i = i % SIZE; // Получаем номер столбца, где находится элемент с индексом i
+
+        // Преобразование индекса j в строку и столбец
+        int row_j = j / SIZE; // Получаем номер строки, где находится элемент с индексом j
+        int col_j = j % SIZE; // Получаем номер столбца, где находится элемент с индексом j
+
+        // Обмен элементов в позициях (row_i, col_i) и (row_j, col_j)
+        swap(&board[row_i][col_i], &board[row_j][col_j]);
     }
 }
 
+// Функция для проверки победы в игре
 int checkWin(int board[SIZE][SIZE]) {
     int count = 1;
     for (int i = 0; i < SIZE; i++) {
@@ -53,26 +65,30 @@ int checkWin(int board[SIZE][SIZE]) {
     return 1;
 }
 
+// Функция для выполнения хода в игре
 void move(int board[SIZE][SIZE], char command) {
-    int x, y;
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            if (board[i][j] == 0) {
-                x = i, y = j;
+    for (int x = 0; x < SIZE; x++) {
+        for (int y = 0; y < SIZE; y++) {
+            if (board[x][y] == 0) {
+                if (command == 'w' && x < SIZE - 1) {
+                    swap(&board[x][y], &board[x + 1][y]);
+                }
+                if (command == 's' && x > 0) {
+                    swap(&board[x][y], &board[x - 1][y]);
+                }
+                if (command == 'a' && y < SIZE - 1) {
+                    swap(&board[x][y], &board[x][y + 1]);
+                }
+                if (command == 'd' && y > 0) {
+                    swap(&board[x][y], &board[x][y - 1]);
+                }
+                return; // Выход из функции после хода
             }
         }
     }
+}
 
-    if (command == 'w' && x < SIZE - 1) {
-        swap(&board[x][y], &board[x + 1][y]);
-    }
-    if (command == 's' && x > 0) {
-        swap(&board[x][y], &board[x - 1][y]);
-    }
-    if (command == 'a' && y < SIZE - 1) {
-        swap(&board[x][y], &board[x][y + 1]);
-    }
-    if (command == 'd' && y > 0) {
-        swap(&board[x][y], &board[x][y - 1]);
-    }
+// Функция для вывода количества ходов
+void displayMoves(int moves) {
+    printf("Количество ходов: %d\n", moves);
 }
