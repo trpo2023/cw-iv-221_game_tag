fcfe4f2b486f33ff8959e3865306503fad43d73d
plesha <124110301+p5tmm@users.noreply.github.com>
2023-08-20 19:57:40 +0600

    Add updated file
    Added screen cleaning. 
    Removed time - After the end of the game, it should show the time, it often did not work correctly, we decided to remove the time display from the project.
    Did formatting

--- main.c
+++ main.c
@@ -1,41 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include "game.h"
+#include <time.h>
 
 #define SIZE 4
 
 int main() {
-    int board[SIZE][SIZE] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0}};
+    // Создание начальной раскладки доски
+    int board[SIZE][SIZE] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 0}
+    };
+
+    // Инициализация генератора случайных чисел на основе текущего времени
+    srand(time(NULL));
+
+    // Перемешивание начальной раскладки доски перед началом игры
     shuffle(board);
 
-    time_t startTime = time(NULL);
-    time_t currentTime;
-    int minutes = 0;
-    int hours = 0;
-    int moves = 0;
+    int moves = 0; // Инициализация счетчика ходов
 
     while (1) {
-        currentTime = time(NULL);
-        int elapsedTime = (int)(currentTime - startTime);
-        int seconds = elapsedTime % 60;
-
-        if (seconds == 0 && elapsedTime > 0) {
-            minutes++;
-            if (minutes == 60) {
-                hours++;
-                minutes = 0;
-            }
-        }
-
-        system("clear");
-        printBoard(board);
+        system("clear"); // Очистка экрана (для UNIX-подобных систем)
+        printBoard(board); // Вывод текущей раскладки доски
         printf("'w' - вверх\n's' - вниз\n'a' - влево\n'd' - вправо\n'q' - выход\n");
 
         if (checkWin(board)) {
             printf("Поздравляем! Вы выиграли!\n");
-            printf("Время: %02d:%02d:%02d\n", hours, minutes, seconds);
-            printf("Количество ходов: %d\n", moves);
+            displayMoves(moves); // Вывод количества ходов
             break;
         }
 
@@ -44,19 +38,13 @@ int main() {
 
         if (command == 'q') {
             printf("Выход из игры.\n");
-            currentTime = time(NULL);
-            elapsedTime = (int)(currentTime - startTime);
-            seconds = elapsedTime % 60;
-            minutes = (int)((elapsedTime / 60) % 60);
-            hours = (int)(elapsedTime / 3600);
-            printf("Время: %02d:%02d:%02d\n", hours, minutes, seconds);
-            printf("Количество ходов: %d\n", moves);
+            displayMoves(moves); // Вывод количества ходов
             break;
         }
 
         if (command == 'w' || command == 'a' || command == 's' || command == 'd') {
-            move(board, command);
-            moves++;
+            move(board, command); // Выполнение хода
+            moves++; // Увеличение счетчика ходов
         }
     }
 
