808dd9799bf977bd9496275884bbce6c2fb08346
ZemeLya-byte <124696107+ZemeLya-byte@users.noreply.github.com>
2023-08-21 16:15:32 +0700

    Updated game_test.c
    Updated "shuffle" function
    Did the formatting
    Made tests for "move"

--- game_test.c
+++ game_test.c
@@ -1,8 +1,9 @@
 #include "unity.h"
 #include "game.h"
+#include <assert.h>
 
-void setUp(void) { /* Do nothing */ }
-void tearDown(void) { /* Do nothing */ }
+void setUp(void) { /* Ничего не делаем */ }
+void tearDown(void) { /* Ничего не делаем */ }
 
 void test_swap(void) {
     int a = 5;
@@ -12,32 +13,107 @@ void test_swap(void) {
     TEST_ASSERT_EQUAL_INT(5, b);
 }
 
+/*
+  Тестирование функции checkWin() для проверки условия победы на начальном поле.
+  Начальное поле содержит числа от 1 до SIZE*SIZE-1 и 0 в последней ячейке.
+  Ожидается, что функция вернет 1 (успешную победу).
+*/
 void test_checkWin(void) {
-    int board1[SIZE][SIZE] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0}};
+    int board1[SIZE][SIZE] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 0}
+    };
     TEST_ASSERT_EQUAL_INT(1, checkWin(board1));
 
-    int board2[SIZE][SIZE] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 0, 15}};
+    int board2[SIZE][SIZE] = {
+        {1, 2, 3, 4}, 
+        {5, 6, 7, 8}, 
+        {9, 10, 11, 12}, 
+        {13, 14, 0, 15}
+    };
     TEST_ASSERT_EQUAL_INT(0, checkWin(board2));
 }
 
 void test_shuffle(void) {
-    int board[SIZE][SIZE] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0}};
-    shuffle(board);
+    int board[SIZE][SIZE] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 0}
+    };
 
-    int seen[SIZE * SIZE] = {0};
-
+    // Копируем оригинальное поле для сравнения после перемешивания
+    int original_board[SIZE][SIZE];
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
-            int number = board[i][j];
-            TEST_ASSERT(number >= 0 && number < SIZE * SIZE);
-            TEST_ASSERT(seen[number] == 0);
-            seen[number] = 1;
+            original_board[i][j] = board[i][j];
         }
     }
 
-    for (int i = 0; i < SIZE * SIZE; i++) {
-        TEST_ASSERT(seen[i] == 1);
+    shuffle(board);
+
+    // Проверяем, что поле изменилось после перемешивания
+    int same = 1;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (board[i][j] != original_board[i][j]) {
+                same = 0;
+                break;
+            }
+        }
     }
+
+    TEST_ASSERT_FALSE(same);
+}
+
+void test_move_up() {
+    int board[SIZE][SIZE] = {{1, 2, 3, 4},
+                             {5, 0, 6, 7},
+                             {8, 9, 10, 11},
+                             {12, 13, 14, 15}};
+    
+    // Проверка движения вверх
+    move(board, 'w');
+    assert(board[1][1] == 9);  // Проверяем, что элемент переместился вверх
+    assert(board[2][1] == 0);  // Проверяем, что пустая ячейка заняла место элемента
+}
+
+void test_move_down() {
+    int board[SIZE][SIZE] = {{1, 2, 3, 4},
+                             {5, 0, 6, 7},
+                             {8, 9, 10, 11},
+                             {12, 13, 14, 15}};
+    
+    // Проверка движения вниз
+    move(board, 's');
+    assert(board[0][1] == 0);  // Проверяем, что пустая ячейка заняла место элемента
+    assert(board[1][1] == 2);  // Проверяем, что элемент переместился вниз
+}
+
+void test_move_left() {
+    int board[SIZE][SIZE] = {{1, 2, 3, 4},
+                             {5, 0, 6, 7},
+                             {8, 9, 10, 11},
+                             {12, 13, 14, 15}};
+    
+    // Проверка движения влево
+    move(board, 'a');
+    assert(board[1][2] == 0);  // Проверяем, что пустая ячейка заняла место элемента
+    assert(board[1][1] == 6);  // Проверяем, что элемент переместился влево
+}
+
+void test_move_right() {
+    int board[SIZE][SIZE] = {{1, 2, 3, 4},
+                             {5, 0, 6, 7},
+                             {8, 9, 10, 11},
+                             {12, 13, 14, 15}};
+    
+    // Проверка движения вправо
+    move(board, 'd');
+    assert(board[1][1] == 5);  // Проверяем, что элемент переместился вправо
+    assert(board[1][0] == 0);  // Проверяем, что пустая ячейка заняла место элемента
 }
 
 int main(void) {
@@ -45,6 +121,10 @@ int main(void) {
     RUN_TEST(test_swap);
     RUN_TEST(test_checkWin);
     RUN_TEST(test_shuffle);
+    RUN_TEST(test_move_up);
+    RUN_TEST(test_move_down);
+    RUN_TEST(test_move_left);
+    RUN_TEST(test_move_right);
     UNITY_END();
 
     return 0;
