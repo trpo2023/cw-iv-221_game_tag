15784765418c9240b35fd4335bf6052cc7e4d086
Михаил Плешков <pl3shkov.m@yandex.ru>
2023-09-28 12:02:16 +0700

    Add clang-format

--- src/game.c
+++ src/game.c
@@ -6,89 +6,91 @@
 
 // Функция для вывода игровой доски на экран
 void printBoard(int board[SIZE][SIZE]) {
-    printf("┌─────┬─────┬─────┬─────┐\n");
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            if (board[i][j] == 0) {
-                printf("│%4c ", ' ');
-            } else {
-                printf("│%4d ", board[i][j]);
-            }
-        }
-        printf("│\n");
-        if (i != SIZE - 1) {
-            printf("├─────┼─────┼─────┼─────┤\n");
-        }
+  printf("┌─────┬─────┬─────┬─────┐\n");
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j < SIZE; j++) {
+      if (board[i][j] == 0) {
+        printf("│%4c ", ' ');
+      } else {
+        printf("│%4d ", board[i][j]);
+      }
     }
-    printf("└─────┴─────┴─────┴─────┘\n\n");
+    printf("│\n");
+    if (i != SIZE - 1) {
+      printf("├─────┼─────┼─────┼─────┤\n");
+    }
+  }
+  printf("└─────┴─────┴─────┴─────┘\n\n");
 }
 
 // Функция для обмена значениями двух переменных
-void swap(int* a, int* b) {
-    int t = *a;
-    *a = *b;
-    *b = t;
+void swap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
 }
 
 // Функция для перемешивания игровой доски
 void shuffle(int board[SIZE][SIZE]) {
-    for (int i = SIZE * SIZE - 1; i > 0; i--) {
-        int j = rand() % (i + 1);
+  for (int i = SIZE * SIZE - 1; i > 0; i--) {
+    int j = rand() % (i + 1);
 
-        // Преобразование индекса i в строку и столбец
-        int row_i = i / SIZE; // Получаем номер строки, где находится элемент с индексом i                               
-        int col_i = i % SIZE; // Получаем номер столбца, где находится элемент с индексом i
+    // Преобразование индекса i в строку и столбец
+    int row_i =
+        i / SIZE; // Получаем номер строки, где находится элемент с индексом i
+    int col_i =
+        i % SIZE; // Получаем номер столбца, где находится элемент с индексом i
 
-        // Преобразование индекса j в строку и столбец
-        int row_j = j / SIZE; // Получаем номер строки, где находится элемент с индексом j
-        int col_j = j % SIZE; // Получаем номер столбца, где находится элемент с индексом j
+    // Преобразование индекса j в строку и столбец
+    int row_j =
+        j / SIZE; // Получаем номер строки, где находится элемент с индексом j
+    int col_j =
+        j % SIZE; // Получаем номер столбца, где находится элемент с индексом j
 
-        // Обмен элементов в позициях (row_i, col_i) и (row_j, col_j)
-        swap(&board[row_i][col_i], &board[row_j][col_j]);
-    }
+    // Обмен элементов в позициях (row_i, col_i) и (row_j, col_j)
+    swap(&board[row_i][col_i], &board[row_j][col_j]);
+  }
 }
 
 // Функция для проверки победы в игре
 int checkWin(int board[SIZE][SIZE]) {
-    int count = 1;
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            if (board[i][j] != count) {
-                return 0;
-            }
-            count++;
-            if (count == SIZE * SIZE) {
-                count = 0;
-            }
-        }
+  int count = 1;
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j < SIZE; j++) {
+      if (board[i][j] != count) {
+        return 0;
+      }
+      count++;
+      if (count == SIZE * SIZE) {
+        count = 0;
+      }
     }
-    return 1;
+  }
+  return 1;
 }
 
 // Функция для выполнения хода в игре
 void move(int board[SIZE][SIZE], char command) {
-    for (int x = 0; x < SIZE; x++) {
-        for (int y = 0; y < SIZE; y++) {
-            if (board[x][y] == 0) {
-                if (command == 'w' && x < SIZE - 1) {
-                    swap(&board[x][y], &board[x + 1][y]);
-                }
-                if (command == 's' && x > 0) {
-                    swap(&board[x][y], &board[x - 1][y]);
-                }
-                if (command == 'a' && y < SIZE - 1) {
-                    swap(&board[x][y], &board[x][y + 1]);
-                }
-                if (command == 'd' && y > 0) {
-                    swap(&board[x][y], &board[x][y - 1]);
-                }
-                return; // Выход из функции после хода
-            }
+  for (int x = 0; x < SIZE; x++) {
+    for (int y = 0; y < SIZE; y++) {
+      if (board[x][y] == 0) {
+        if (command == 'w' && x < SIZE - 1) {
+          swap(&board[x][y], &board[x + 1][y]);
         }
+        if (command == 's' && x > 0) {
+          swap(&board[x][y], &board[x - 1][y]);
+        }
+        if (command == 'a' && y < SIZE - 1) {
+          swap(&board[x][y], &board[x][y + 1]);
+        }
+        if (command == 'd' && y > 0) {
+          swap(&board[x][y], &board[x][y - 1]);
+        }
+        return; // Выход из функции после хода
+      }
     }
+  }
 }
 
 // Функция для вывода количества ходов
-void displayMoves(int moves) {
-    printf("Количество ходов: %d\n", moves);
-}
+void displayMoves(int moves) { printf("Количество ходов: %d\n", moves); }
--- tests/game_test.c
+++ tests/game_test.c
@@ -1,16 +1,18 @@
-#include "unity.h"
 #include "game.h"
+#include "unity.h"
 #include <assert.h>
 
-void setUp(void) { /* Ничего не делаем */ }
-void tearDown(void) { /* Ничего не делаем */ }
+void setUp(void) { /* Ничего не делаем */
+}
+void tearDown(void) { /* Ничего не делаем */
+}
 
 void test_swap(void) {
-    int a = 5;
-    int b = 10;
-    swap(&a, &b);
-    TEST_ASSERT_EQUAL_INT(10, a);
-    TEST_ASSERT_EQUAL_INT(5, b);
+  int a = 5;
+  int b = 10;
+  swap(&a, &b);
+  TEST_ASSERT_EQUAL_INT(10, a);
+  TEST_ASSERT_EQUAL_INT(5, b);
 }
 
 /*
@@ -19,113 +21,97 @@ void test_swap(void) {
   Ожидается, что функция вернет 1 (успешную победу).
 */
 void test_checkWin(void) {
-    int board1[SIZE][SIZE] = {
-        {1, 2, 3, 4},
-        {5, 6, 7, 8},
-        {9, 10, 11, 12},
-        {13, 14, 15, 0}
-    };
-    TEST_ASSERT_EQUAL_INT(1, checkWin(board1));
+  int board1[SIZE][SIZE] = {
+      {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0}};
+  TEST_ASSERT_EQUAL_INT(1, checkWin(board1));
 
-    int board2[SIZE][SIZE] = {
-        {1, 2, 3, 4}, 
-        {5, 6, 7, 8}, 
-        {9, 10, 11, 12}, 
-        {13, 14, 0, 15}
-    };
-    TEST_ASSERT_EQUAL_INT(0, checkWin(board2));
+  int board2[SIZE][SIZE] = {
+      {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 0, 15}};
+  TEST_ASSERT_EQUAL_INT(0, checkWin(board2));
 }
 
 void test_shuffle(void) {
-    int board[SIZE][SIZE] = {
-        {1, 2, 3, 4},
-        {5, 6, 7, 8},
-        {9, 10, 11, 12},
-        {13, 14, 15, 0}
-    };
+  int board[SIZE][SIZE] = {
+      {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0}};
 
-    // Копируем оригинальное поле для сравнения после перемешивания
-    int original_board[SIZE][SIZE];
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            original_board[i][j] = board[i][j];
-        }
+  // Копируем оригинальное поле для сравнения после перемешивания
+  int original_board[SIZE][SIZE];
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j < SIZE; j++) {
+      original_board[i][j] = board[i][j];
     }
+  }
 
-    shuffle(board);
+  shuffle(board);
 
-    // Проверяем, что поле изменилось после перемешивания
-    int same = 1;
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            if (board[i][j] != original_board[i][j]) {
-                same = 0;
-                break;
-            }
-        }
+  // Проверяем, что поле изменилось после перемешивания
+  int same = 1;
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j < SIZE; j++) {
+      if (board[i][j] != original_board[i][j]) {
+        same = 0;
+        break;
+      }
     }
+  }
 
-    TEST_ASSERT_FALSE(same);
+  TEST_ASSERT_FALSE(same);
 }
 
 void test_move_up() {
-    int board[SIZE][SIZE] = {{1, 2, 3, 4},
-                             {5, 0, 6, 7},
-                             {8, 9, 10, 11},
-                             {12, 13, 14, 15}};
-    
-    // Проверка движения вверх
-    move(board, 'w');
-    assert(board[1][1] == 9);  // Проверяем, что элемент переместился вверх
-    assert(board[2][1] == 0);  // Проверяем, что пустая ячейка заняла место элемента
+  int board[SIZE][SIZE] = {
+      {1, 2, 3, 4}, {5, 0, 6, 7}, {8, 9, 10, 11}, {12, 13, 14, 15}};
+
+  // Проверка движения вверх
+  move(board, 'w');
+  assert(board[1][1] == 9); // Проверяем, что элемент переместился вверх
+  assert(board[2][1] ==
+         0); // Проверяем, что пустая ячейка заняла место элемента
 }
 
 void test_move_down() {
-    int board[SIZE][SIZE] = {{1, 2, 3, 4},
-                             {5, 0, 6, 7},
-                             {8, 9, 10, 11},
-                             {12, 13, 14, 15}};
-    
-    // Проверка движения вниз
-    move(board, 's');
-    assert(board[0][1] == 0);  // Проверяем, что пустая ячейка заняла место элемента
-    assert(board[1][1] == 2);  // Проверяем, что элемент переместился вниз
+  int board[SIZE][SIZE] = {
+      {1, 2, 3, 4}, {5, 0, 6, 7}, {8, 9, 10, 11}, {12, 13, 14, 15}};
+
+  // Проверка движения вниз
+  move(board, 's');
+  assert(board[0][1] ==
+         0); // Проверяем, что пустая ячейка заняла место элемента
+  assert(board[1][1] == 2); // Проверяем, что элемент переместился вниз
 }
 
 void test_move_left() {
-    int board[SIZE][SIZE] = {{1, 2, 3, 4},
-                             {5, 0, 6, 7},
-                             {8, 9, 10, 11},
-                             {12, 13, 14, 15}};
-    
-    // Проверка движения влево
-    move(board, 'a');
-    assert(board[1][2] == 0);  // Проверяем, что пустая ячейка заняла место элемента
-    assert(board[1][1] == 6);  // Проверяем, что элемент переместился влево
+  int board[SIZE][SIZE] = {
+      {1, 2, 3, 4}, {5, 0, 6, 7}, {8, 9, 10, 11}, {12, 13, 14, 15}};
+
+  // Проверка движения влево
+  move(board, 'a');
+  assert(board[1][2] ==
+         0); // Проверяем, что пустая ячейка заняла место элемента
+  assert(board[1][1] == 6); // Проверяем, что элемент переместился влево
 }
 
 void test_move_right() {
-    int board[SIZE][SIZE] = {{1, 2, 3, 4},
-                             {5, 0, 6, 7},
-                             {8, 9, 10, 11},
-                             {12, 13, 14, 15}};
-    
-    // Проверка движения вправо
-    move(board, 'd');
-    assert(board[1][1] == 5);  // Проверяем, что элемент переместился вправо
-    assert(board[1][0] == 0);  // Проверяем, что пустая ячейка заняла место элемента
+  int board[SIZE][SIZE] = {
+      {1, 2, 3, 4}, {5, 0, 6, 7}, {8, 9, 10, 11}, {12, 13, 14, 15}};
+
+  // Проверка движения вправо
+  move(board, 'd');
+  assert(board[1][1] == 5); // Проверяем, что элемент переместился вправо
+  assert(board[1][0] ==
+         0); // Проверяем, что пустая ячейка заняла место элемента
 }
 
 int main(void) {
-    UNITY_BEGIN();
-    RUN_TEST(test_swap);
-    RUN_TEST(test_checkWin);
-    RUN_TEST(test_shuffle);
-    RUN_TEST(test_move_up);
-    RUN_TEST(test_move_down);
-    RUN_TEST(test_move_left);
-    RUN_TEST(test_move_right);
-    UNITY_END();
+  UNITY_BEGIN();
+  RUN_TEST(test_swap);
+  RUN_TEST(test_checkWin);
+  RUN_TEST(test_shuffle);
+  RUN_TEST(test_move_up);
+  RUN_TEST(test_move_down);
+  RUN_TEST(test_move_left);
+  RUN_TEST(test_move_right);
+  UNITY_END();
 
-    return 0;
+  return 0;
 }
